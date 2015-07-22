1309232978d16e63c99335ff33fe1f45f202d146
j neg <javon.negahban@cox.net>
2015-07-22 15:25:46 -0700

    Fixed initial directory with whitespace bug

--- uncapextensions.sh
+++ uncapextensions.sh
@@ -5,7 +5,7 @@ EXT=$2
 
 echo "Uncapitalizing ${EXT} extensions recursively starting at ${DIR}..."
 
-find ${DIR} -type f -iname "*.${EXT}" -print0 | while read -r -d $'\0' file
+find "$DIR" -type f -iname "*.${EXT}" -print0 | while read -r -d $'\0' file
 do
    if [[ ! "$file" =~ .*\.${EXT}$ ]]
    then
