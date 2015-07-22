8f238a51021122a03a155cbeaff2954a137973a6
j neg <javon.negahban@cox.net>
2015-07-22 15:22:54 -0700

    Added initial script

---
+++ uncapextensions.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DIR=$1
+EXT=$2
+
+echo "Uncapitalizing ${EXT} extensions recursively starting at ${DIR}..."
+
+find ${DIR} -type f -iname "*.${EXT}" -print0 | while read -r -d $'\0' file
+do
+   if [[ ! "$file" =~ .*\.${EXT}$ ]]
+   then
+      echo "Modifying ${file}..."
+      mv "$file" "`echo "$file" | sed "s/\.${EXT}$/.${EXT}/I"`"
+   fi
+done
